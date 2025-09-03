e3d5b4cd57f629b7c8bbc4fbdffb8dbe38eeb1dd
gobtronic <1554705+gobtronic@users.noreply.github.com>
2025-09-03 08:03:36 +0200

    refactor how setup error are handled + add setup command

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,50 @@
 mod config;
 mod log;
+mod setup;
 mod wow;
 
-use std::{path::PathBuf, process::exit};
+use std::process::exit;
 
 use anyhow::Result;
+use clap::{Parser, Subcommand};
 use config::Config;
-use dialoguer::{Input, theme::ColorfulTheme};
 use log::*;
 
-fn main() -> Result<()> {
-    let mut cfg: Config = confy::load("kekrs", "config")?;
-    if cfg.wow_dir_path.as_os_str() == "" {
-        ilog("You don't have any WoW installation linked yet, will run setup.");
-        let wow_dir_path = prompt_wow_dir_path();
-        if !wow::is_valid_wow_dir_path(&wow_dir_path, true) {
-            exit(1);
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// Setup your WoW directory
+    Setup,
+}
+
+fn main() {
+    let cli = Args::parse();
+    let _ = run(cli).map_err(|err| {
+        log::elog(err);
+        exit(1);
+    });
+}
+
+fn run(cli: Args) -> Result<()> {
+    let mut cfg: Config = config::load()?;
+    match &cli.command {
+        Some(Commands::Setup) => {
+            setup::run(&mut cfg)?;
         }
-        cfg.wow_dir_path = wow_dir_path;
-        confy::store("kekrs", "config", cfg)?;
+        None => {}
+    }
+
+    let mut cfg: Config = config::load()?;
+    if !cfg.is_initialized() {
+        ilog("You don't have any WoW installation linked yet, will run setup.");
+        setup::run(&mut cfg)?;
     }
 
     Ok(())
-}
-
-fn prompt_wow_dir_path() -> PathBuf {
-    let input: String = Input::with_theme(&ColorfulTheme::default())
-        .with_prompt("WoW directory path")
-        .interact_text()
-        .unwrap();
-
-    input.into()
 }
